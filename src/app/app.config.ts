9d2631c2d71db027d49a163f71574b00815452ec
Marlon KUQI <marlon.kuqi@capgemini.com>
2025-06-29 14:55:14 +0200

    Ajout d'une login page Reste des erreurs à corriger

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(BrowserModule, ReactiveFormsModule),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {LoginFormComponent} from './pages/auth/login-form/login-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginFormComponent },
+];
