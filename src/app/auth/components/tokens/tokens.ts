c15de579de3c755b068c358bf21b7872be66aead
Marlon KUQI <marlon.kuqi@capgemini.com>
2025-06-29 12:38:27 +0200

    Ajout composant tokens

---
+++ src/app/auth/components/tokens/tokens.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tokens',
+  imports: [],
+  templateUrl: './tokens.html',
+  styleUrl: './tokens.css'
+})
+export class Tokens {
+
+}
